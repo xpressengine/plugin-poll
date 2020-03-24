b6139b8b87d13295b4f0ab16bfd354a5181a6b30
xedev <developers@xpressengine.com>
2020-03-24 10:58:44 +0900

    설문 사용자 화면 스타일 수정

--- views/user/default.blade.php
+++ views/user/default.blade.php
@@ -1,31 +1,58 @@
 @if ($javascriptInit == true)
 <style>
-    .__xe_poll_section {max-width: 500px;}
-    .poll-message {background-color:#D9EDF7; color:#3A87AD; border-color:#BCE8F1;}
-    .poll-header {background-color:#444; color:#fff;}
-    .poll-body {border:1px solid #444; padding: 5px 10px;}
-    .poll-body {border:1px solid #444; padding: 5px 10px;}
+    .__xe_poll_section {max-width: 500px;}/*<- 수정 불가 */
+    
+    /* 설문지 UI 수정 */
+    .poll-message {background-color:#D9EDF7; color:#3A87AD; border-color:#BCE8F1}
+
+    .poll-body {background-color:#f8f9fb;width:500px; padding: 20px;border-radius:20px;box-shadow:5px 5px 10px #efefef;border:1px solid #e3e3e3;}
+    /* .poll-body {border:1px solid #444; padding: 5px 10px;} */
+    .poll-header .title{ font-size:20px;text-align:center;font-weight:bold}
+    .poll-header .date{ font-size:14px;text-align:center}
+    
     .poll-body ul { list-style:none;}
     .progress{background-color:#dee2e6;height:20px; width:100%;}
     .progress-bar{background-color:#17a2b8;height:20px; width:0%;}
 
-    .poll-question {border-bottom:1px solid #888; font-weight:bold;}
+    .poll-question {border-bottom:1px solid #888;font-weight: 400;}
+    .item .poll-question {border-bottom:1px solid #888;font-weight: 400;}
+    
+    .poll-footer {text-align:center;padding-top:40px;}
+
+    .poll-header .date{font-weight:500;margin-bottom:36px;}
+
+    
+
     /*.poll-option ul {margin-top: 3px;}*/
+    .poll-option {padding-left: 0; }
     .poll-option li:nth-last-child(1) {margin-bottom: 10px;}
+    .poll-option .progress,.progress-bar{border-radius:20px;padding-bottom:12px}
+    .poll-option li {position:relative}
+    .poll-option li label .title {position:absolute;top:0;left:16px;}
+
+    .poll-header .poll-header__sub-text {font-size:14px;}
+    .xe_poll_survey_result .poll-option {margin-bottom:0}
+    .xe_poll_survey_result .poll-option li {margin-bottom:14px}
+   
+    
+    
+
+    /* 설문지 UI 수정 */
+
 </style>
 @endif
 <div class="__xe_poll_section">
     @if ($isExpired == false && $isJoined == false)
-    <div class="__xe_poll_survey" data-id="{{$poll->id}}">
+    <div class="__xe_poll_survey xe_poll_survey" data-id="{{$poll->id}}">
         <form class="__xe_poll_form" method="post" action="{{route('xe_poll::editor_tool.join', ['id' => $poll->id])}}">
             <input type="hidden" name="return_url" value="{{$current_url}}" />
             <input type="hidden" name="id" value="{{$poll->id}}" />
             <div class="poll-body">
                 <div class="poll-header">
-                    <p class="title">투표 : {{$poll->title}} (참여자: {{$poll->poll_count}})</p>
+                    <p class="title">투표 : {{$poll->title}} <b class="poll-header__sub-text">(참여자: {{$poll->poll_count}})</b></p>
                     <p class="date">설문조사 종료일 : {{$poll->expired_at->format('Y-m-d')}}</p>
                 </div>
-
+                
                 @foreach($poll->items as $item)
                 <div class="poll-item" data-item-id="{{$item->id}}" data-check-count="{{$item->check_count}}">
                     <p class="poll-question">질문 : {{$item->title}} @if ($item->check_count > 1)({{$item->check_count}}개 선택)@endif</p>
@@ -33,18 +60,20 @@
                     <ul class="poll-option">
                         @foreach($item->options as $option)
                             <li>
-                                @if ($item->check_count > 1)
-                                    <input type="checkbox" class="select_option" name="option_{{$item->id}}[]" value="{{$option->id}}"/>
-                                @else
-                                    <input type="radio" class="select_option" name="option_{{$item->id}}[]" value="{{$option->id}}"/>
-                                @endif
-                                <span class="title">{{$option->title}}</span>
+                                <label for="">
+                                    @if ($item->check_count > 1)
+                                        <input type="checkbox" class="select_option " name="option_{{$item->id}}[]" value="{{$option->id}}"/>
+                                    @else
+                                        <input type="radio" class="select_option" name="option_{{$item->id}}[]" value="{{$option->id}}"/>
+                                    @endif
+                                    <span class="title">{{$option->title}}</span>
+                                </label>
                             </li>
                         @endforeach
                     </ul>
                 </div>
                 @endforeach
-
+               
                 <div class="poll-footer">
                     <button type="button" class="xe-btn xe-btn-normal btn-goto-result">결과 보기</button>
                     <button type="submit" class="xe-btn xe-btn-primary">설문 참여</button>
@@ -54,7 +83,7 @@
         </form>
     </div>
     @endif
-    <div class="__xe_poll_survey_result" data-id="{{$poll->id}}" @if ($isExpired == false && $isJoined == false)style="display:none;"@endif >
+    <div class="__xe_poll_survey_result xe_poll_survey_result" data-id="{{$poll->id}}" @if ($isExpired == false && $isJoined == false)style="display:none;"@endif >
         @if ($isExpired == true)
             <p class="poll-expired poll-message">투표가 종료되었습니다.</p>
         @elseif ($isJoined == true)
@@ -63,7 +92,7 @@
 
         <div class="poll-body">
             <div class="poll-header">
-                <p class="title">투표 : {{$poll->title}} (참여자: {{$poll->poll_count}})</p>
+                <p class="title">투표 : {{$poll->title}} <b class="poll-header__sub-text">(참여자: {{$poll->poll_count}})<b></p>
                 <p class="date">설문조사 종료일 : {{$poll->expired_at->format('Y-m-d')}}</p>
             </div>
 
